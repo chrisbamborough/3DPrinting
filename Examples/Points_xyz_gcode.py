532d3e2ee0570171223715345b7ccdcc9af491e9
Christopher Bamborough <smthspce@macbookpro.lan>
2025-01-10 09:36:07 +1100

    Add crealty examples

---
+++ Examples/Points_xyz_gcode.py
@@ -0,0 +1,20 @@
+import rhinoscriptsyntax as rs
+
+
+
+
+
+
+
+# take in or create a curve through contour 
+# extract points
+# define the bed size
+# set the print layer height
+
+# set extrusion ratio
+
+# deconstruction points
+# concatenate point coordiantes with GCode
+# add extrusion values
+# add start code
+# add end code
